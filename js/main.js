bad87783927b4be1496a4fd5f9d8a5e9cdce33e2
Miku Nagakawa <149560996+nagakawa39@users.noreply.github.com>
2024-10-08 23:54:48 +0900

    added functions to display select elements

--- js/main.js
+++ js/main.js
@@ -39,11 +39,11 @@ const config = {
         "benchmark": 0
     },
     count: 1,
-    
+    CPU: []
 }
 
 class Computer{
-    componentsName = ["cpu", "gpu", "ram", "storage"];
+    static componentsName = ["cpu", "gpu", "ram", "storage"];
     components = {
         cpu: null,
         gpu: null,
@@ -114,8 +114,25 @@ class Storage extends Component{
 
 class Controller{
     #computer = null;
+    static componentsData = {
+        "cpu": null,
+        "gpu": null,
+        "ram": null,
+        "hdd": null,
+        "ssd": null
+    }
+
     getStorageCapacity(){
 
+    }
+
+    static getComponentData(){
+        for (let component in this.componentsData){
+            fetch(config.url+component).then(response=>response.json()).then(function(data){
+                console.log(this.componentsData);
+                // this.componentsData[component] = data;
+            });
+        }
     }
 
     /* computerオブジェクトを関数内で定義していることに対する違和感あり。
@@ -146,6 +163,192 @@ class Controller{
 }
 
 class View{
+    static componentLabels = {
+        "cpu": ["Brand", "Model"],
+        "gpu": ["Brand", "Model"],
+        "ram": ["How_Many", "Brand", "Model"],
+        "storage": ["HDD or SSD", "Storage", "Brand", "Model"],
+    };
+
+    // オプションを作成
+    static generateDropdownOptions(optionArr) {
+
+        let options = `<option>-</option>`;
+    
+        for (let i in optionArr) {
+            options += `<option value='${optionArr[i]}'>${optionArr[i]}</option>`;
+        }
+        // console.log(options);
+        return options;
+    }
+
+    // select要素を作成
+    static generateSelectEle(label, component){
+        let formGroup = document.createElement("div");
+        formGroup.classList.add("form-group", "col-lg-3");
+        let labelEle = document.createElement("label");
+        labelEle.innerHTML = label;
+
+        let selectEle = document.createElement("select");
+        selectEle.classList.add("form-control");
+        selectEle.id = component + label + "Select";
+
+        /* オプションを作成するめの配列を受け取る */
+        let optionArr = ["aaa", "bbb", "ccc"];
+        let options = this.generateDropdownOptions(optionArr);
+        // console.log(options);
+        selectEle.innerHTML = options;
+        formGroup.append(labelEle);
+        formGroup.append(selectEle);
+        // console.log(selectEle);
+        return formGroup;
+    }
+
+    // 各パーツのselection要素を含んだカードを作成
+    static generateComponentSelectionCard(componentName){
+        // タイトル
+        let title = document.createElement("h4");
+        title.classList.add("mt-4");
+        title.innerHTML = componentName.toUpperCase();
+
+        let row = document.createElement("div");
+        row.classList.add("form-row", "border", "border-warning");
+
+        let target = document.getElementById("target");
+        
+
+        for (let i in this.componentLabels[componentName]){
+            let selectEle = this.generateSelectEle(this.componentLabels[componentName][i], componentName);
+            row.append(selectEle);
+        }
+
+        target.append(title);
+        target.append(row);
+    }
+
+    static generateSelectPage(){
+        let container = document.createElement('div');
+        container.classList.add("select-section", "col-12");
+
+
+        let ex =
+        `
+                    <!-- CPUセクション -->
+                    <h4 class="mt-4">step1: Select Your CPU</h4>
+                    <div class="row">
+                        <div class="div col-3">
+                            <label for="cpuBrandSelect" class="col-1">Brand</label>
+                            <select class="form-control" id="cpuBrandSelect" required>
+                                <option value="none">-</option>
+                                <option value="Intel">Intel</option>
+                                <option value="AMD">AMD</option>
+                            </select>
+                        </div>
+                        <div class="col-3">
+                            <label for="cpuModelSelect" class="col-1">Model</label>
+                            <select class="form-control" id="cpuModelSelect" required>
+                                <option>-</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!-- GPUセクション -->
+                    <h4 class="mt-4">step2: Select Your GPU</h4>
+                    <div class="row">
+                        <label for="gpuBrandSelect" class="col-1">Brand</label>
+                        <div class="div col-3">
+                            <select class="form-control" id="gpuBrandSelect" required>
+                                <option>-</option>
+                                <option>Nvidia</option>
+                                <option>AMD</option>
+                                <option>Zotac</option>
+                                <option>Asus</option>
+                                <option>MSI</option>
+                                <option>Gigabyte</option>
+                                <option>EVGA</option>
+                                <option>Sapphire</option>
+                                <option>PowerColor</option>
+                                <option>XFX</option>
+                                <option>ASRock</option>
+                                <option>Gainward</option>
+                                <option>PNY</option>
+                                <option>PwrHis</option>
+                            </select>
+                        </div>
+                        <label for="gpuModelSelect" class="col-1">Model</label>
+                        <div class="col-3">
+                            <select class="form-control" id="gpuModelSelect" required>
+                                <option>-</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!-- ramセクション -->
+                    <h4 class="mt-4">step3: Select Your Memory Card</h4>
+                    <div class="row">
+                        <label for="ramNumSelect" class="col-1">How many?</label>
+                        <div class="div col-3">
+                            <select class="form-control" id="ramNumSelect" required>
+                                <option>-</option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                            </select>
+                        </div>
+                        <label for="ramBrandSelect" class="col-1">Brand</label>
+                        <div class="col-3">
+                            <select class="form-control col-12" id="ramBrandSelect" required>
+                                <option>-</option>
+                            </select>
+                        </div>
+                        <label for="ramModelSelect" class="col-1">Model</label>
+                        <div class="col-3">
+                            <select class="form-control" id="ramModelSelect" required>
+                                <option>-</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!-- storageセクション -->
+                    <h4 class="mt-4">step4: Select Your Storage</h4>
+                    <div class="row">
+                        <label for="storageTypeSelect" class="col-1">HDD or SSD</label>
+                        <div class="div col-2">
+                            <select class="form-control" id="storageTypeSelect" required>
+                                <option>-</option>
+                                <option>HDD</option>
+                                <option>SSD</option>
+                            </select>
+                        </div>
+                        <label for="storageCapacitySelect" class="col-1">Storage</label>
+                        <div class="col-2">
+                            <select class="form-control" id="storageCapacitySelect" required>
+                                <option>-</option>
+                            </select>
+                        </div>
+                        <label for="storageBrandSelect" class="col-1">Brand</label>
+                        <div class="col-2">
+                            <select class="form-control" id="storageBrandSelect" required>
+                                <option>-</option>
+                            </select>
+                        </div>
+                        <label for="storageModelSelect" class="col-1">Model</label>
+                        <div class="col-2">
+                            <select class="form-control" id="storageModelSelect" required>
+                                <option>-</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <button class="btn btn-primary px-5 ml-4 mt-4" id="pc-performance-evaluate-btn" type="button">Add PC</button>
+                    </div>
+            </div>
+            <div class="mt-4" id="pc-spec-result">
+                
+            </div>
+        `;
+    }
     // function generateCPUModelOptions(cpuBrand){
     //     cpuModelArr = []
     
@@ -162,16 +365,7 @@ class View{
     //     });
     // };
 
-    function generateDropdownOptions(optionArr) {
 
-        let options = `<option>-</option>`;
-    
-        for (i in optionArr) {
-            options += `<option value='${optionArr[i]}'>${optionArr[i]}</option>`;
-        }
-        // console.log(options);
-        return options;
-    }
 }
 
 
@@ -390,108 +584,108 @@ function generateStorageModelOptions(storageType, capacity, brand){
 /* イベント操作 */
 
 // CPUブランド選択イベント
-let cpuBrandSelect = document.getElementById(config.cpuBrandSelectId);
-cpuBrandSelect.addEventListener('change', function(e){
-    let cpuBrand = e.target.value;
-    // console.log(cpuBrand);
-    config["cpu"]["brand"] = cpuBrand;
-    generateCPUModelOptions(cpuBrand);
-})
+// let cpuBrandSelect = document.getElementById(config.cpuBrandSelectId);
+// cpuBrandSelect.addEventListener('change', function(e){
+//     let cpuBrand = e.target.value;
+//     // console.log(cpuBrand);
+//     config["cpu"]["brand"] = cpuBrand;
+//     generateCPUModelOptions(cpuBrand);
+// })
 
-// CPUモデル選択イベント
-let cpuModelSelect = document.getElementById(config.cpuModelSelectId);
-cpuModelSelect.addEventListener('change', function(e){
-    let cpuModel = e.target.value;
-    // console.log(cpuBrand);
-    config["cpu"]["model"] = cpuModel;
-})
+// // CPUモデル選択イベント
+// let cpuModelSelect = document.getElementById(config.cpuModelSelectId);
+// cpuModelSelect.addEventListener('change', function(e){
+//     let cpuModel = e.target.value;
+//     // console.log(cpuBrand);
+//     config["cpu"]["model"] = cpuModel;
+// })
 
-// GPUブランド選択イベント
-let gpuBrandSelect = document.getElementById(config.gpuBrandSelectId);
-gpuBrandSelect.addEventListener('change', function(e){
-    let gpuBrand = e.target.value;
-    // console.log(gpuBrand);
-    config["gpu"]["brand"] = gpuBrand;
-    generateGPUModelOptions(gpuBrand);
-})
+// // GPUブランド選択イベント
+// let gpuBrandSelect = document.getElementById(config.gpuBrandSelectId);
+// gpuBrandSelect.addEventListener('change', function(e){
+//     let gpuBrand = e.target.value;
+//     // console.log(gpuBrand);
+//     config["gpu"]["brand"] = gpuBrand;
+//     generateGPUModelOptions(gpuBrand);
+// })
 
-// GPUモデル選択イベント
-let gpuModelSelect = document.getElementById(config.gpuModelSelectId);
-gpuModelSelect.addEventListener('change', function(e){
-    let gpuModel = e.target.value;
-    // console.log(gpuBrand);
-    config["gpu"]["model"] = gpuModel;
-})
+// // GPUモデル選択イベント
+// let gpuModelSelect = document.getElementById(config.gpuModelSelectId);
+// gpuModelSelect.addEventListener('change', function(e){
+//     let gpuModel = e.target.value;
+//     // console.log(gpuBrand);
+//     config["gpu"]["model"] = gpuModel;
+// })
 
-// RAM個数選択イベント
-let ramNumSelect = document.getElementById(config.ramNumSelectId);
-ramNumSelect.addEventListener('change', function(e){
-    let ramNum = e.target.value;
-    // console.log(ramNum);
-    generateRAMBrandOptions(ramNum);
-})
+// // RAM個数選択イベント
+// let ramNumSelect = document.getElementById(config.ramNumSelectId);
+// ramNumSelect.addEventListener('change', function(e){
+//     let ramNum = e.target.value;
+//     // console.log(ramNum);
+//     generateRAMBrandOptions(ramNum);
+// })
 
-// RAMブランド選択イベント
-let ramBrandSelect = document.getElementById(config.ramBrandSelectId);
-ramBrandSelect.addEventListener('change', function(e){
-    let ramBrand = e.target.value;
-    config["ram"]["brand"] = ramBrand;
-    generateRAMModelOptions(ramBrand, ramNumSelect.value);
-})
+// // RAMブランド選択イベント
+// let ramBrandSelect = document.getElementById(config.ramBrandSelectId);
+// ramBrandSelect.addEventListener('change', function(e){
+//     let ramBrand = e.target.value;
+//     config["ram"]["brand"] = ramBrand;
+//     generateRAMModelOptions(ramBrand, ramNumSelect.value);
+// })
 
-// RAMモデル選択イベント
-let ramModelSelect = document.getElementById(config.ramModelSelectId);
-ramModelSelect.addEventListener('change', function(e){
-    let ramModel = e.target.value;
-    config["ram"]["model"] = ramModel;
-})
+// // RAMモデル選択イベント
+// let ramModelSelect = document.getElementById(config.ramModelSelectId);
+// ramModelSelect.addEventListener('change', function(e){
+//     let ramModel = e.target.value;
+//     config["ram"]["model"] = ramModel;
+// })
 
-// Storageタイプ選択イベント
-let storageTypeSelect = document.getElementById(config.storageTypeSelectId);
-storageTypeSelect.addEventListener('change', function(e){
-    let storageType = e.target.value;
-    // console.log(storageType);
-    config.storageType = storageType;
-    generateStorageCapacityOptions(storageType);
-})
+// // Storageタイプ選択イベント
+// let storageTypeSelect = document.getElementById(config.storageTypeSelectId);
+// storageTypeSelect.addEventListener('change', function(e){
+//     let storageType = e.target.value;
+//     // console.log(storageType);
+//     config.storageType = storageType;
+//     generateStorageCapacityOptions(storageType);
+// })
 
-// Storage Capacity選択イベント
-let storageCapacitySelect = document.getElementById(config.storageCapacitySelectId);
-storageCapacitySelect.addEventListener('change', function(e){
-    let capacity = e.target.value;
-    generateStorageBrandOptions(storageTypeSelect.value, capacity);
-})
+// // Storage Capacity選択イベント
+// let storageCapacitySelect = document.getElementById(config.storageCapacitySelectId);
+// storageCapacitySelect.addEventListener('change', function(e){
+//     let capacity = e.target.value;
+//     generateStorageBrandOptions(storageTypeSelect.value, capacity);
+// })
 
-// Storage Brand選択イベント
-let storageBrandSelect = document.getElementById(config.storageBrandSelectId);
-storageBrandSelect.addEventListener('change', function(e){
-    let brand = e.target.value;
-    if (storageTypeSelect.value === "HDD"){
-        config["hdd"]["brand"] = brand;
-    } else {
-        config["ssd"]["brand"] = brand;
-    }
-    generateStorageModelOptions(storageTypeSelect.value, storageCapacitySelect.value, brand);
-})
+// // Storage Brand選択イベント
+// let storageBrandSelect = document.getElementById(config.storageBrandSelectId);
+// storageBrandSelect.addEventListener('change', function(e){
+//     let brand = e.target.value;
+//     if (storageTypeSelect.value === "HDD"){
+//         config["hdd"]["brand"] = brand;
+//     } else {
+//         config["ssd"]["brand"] = brand;
+//     }
+//     generateStorageModelOptions(storageTypeSelect.value, storageCapacitySelect.value, brand);
+// })
 
-// Storage Model選択イベント
-let storageModelSelect = document.getElementById(config.storageModelSelectId);
-storageModelSelect.addEventListener('change', function(e){
-    let model = e.target.value;
-    if (storageTypeSelect.value === "HDD"){
-        config["hdd"]["model"] = model;
-    } else {
-        config["ssd"]["model"] = model;
-    }
-})
+// // Storage Model選択イベント
+// let storageModelSelect = document.getElementById(config.storageModelSelectId);
+// storageModelSelect.addEventListener('change', function(e){
+//     let model = e.target.value;
+//     if (storageTypeSelect.value === "HDD"){
+//         config["hdd"]["model"] = model;
+//     } else {
+//         config["ssd"]["model"] = model;
+//     }
+// })
 
-// ボタンが押された時のイベント
-let button = document.getElementById(config.pcPerformanceEvaluateBtnId);
-button.addEventListener("click", function(){
-    if (isAllFormFilledIn()){
-        computePcSpec();
-    }
-})
+// // ボタンが押された時のイベント
+// let button = document.getElementById(config.pcPerformanceEvaluateBtnId);
+// button.addEventListener("click", function(){
+//     if (isAllFormFilledIn()){
+//         computePcSpec();
+//     }
+// })
 
 
 //全ての項目が選択されているかチェックする
@@ -617,3 +811,14 @@ function generatePCSpecCard(gaming, working){
     card.innerHTML = htmlString;
     return card;
 }
+
+View.generateComponentSelectionCard("cpu");
+View.generateComponentSelectionCard("gpu");
+View.generateComponentSelectionCard("ram");
+View.generateComponentSelectionCard("storage");
+
+Controller.getComponentData();
+
+setTimeout(function(){
+    console.log(Controller.componentsData["cpu"]);
+},300);
